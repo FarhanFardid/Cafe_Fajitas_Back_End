b98556b4bc25ad31bfd81db44febae46f6796631
FarhanFardid <farhanfardid009@gmail.com>
2023-05-03 16:59:57 +0600

    individual chef info display

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.get('/', (req,res)=>{
 app.get('/chefs',(req,res)=>{
     res.send(chefs)
 })
+
+app.get('/chefs/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    if(id === 0){
+        res.send(chefs);
+    }
+    else{
+        const selectedChef = chefs.find(chef=> parseInt(chef.id) === id)
+        res.send(selectedChef);
+    }
+})
+
 app.listen(port, () =>{
     console.log("The chef server is running on port :", port )
 })
