3ce6d26cb45ac742ca96d7546d1b9a7cb60a66be
FarhanFardid <farhanfardid009@gmail.com>
2023-05-03 16:21:32 +0600

    chefs data added of 2 person

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const port= 5000;
+const chefs = require('./chefs.json')
 
 app.get('/', (req,res)=>{
     res.send("The chef-recipe server is running ")
 })
-
+app.get('/chefs',(req,res)=>{
+    res.send(chefs)
+})
 app.listen(port, () =>{
     console.log("The chef server is running on port :", port )
 })
